cd32df8eea78f2a3737075500c46f8586fa11440
GamerProPlaysYT <lucasrv847@hotmail.com>
2023-02-12 18:50:53 +0100

    Annotation processing handler.

--- JFA/src/main/java/cf/grcq/processing/AnnotationProcessingHandler.java
+++ JFA/src/main/java/cf/grcq/processing/AnnotationProcessingHandler.java
@@ -1,18 +1,46 @@
 package cf.grcq.processing;
 
+import com.google.auto.service.AutoService;
+
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import java.lang.annotation.Annotation;
-import java.util.List;
 import java.util.Set;
 
+@AutoService(Processor.class)
 public abstract class AnnotationProcessingHandler<T> extends AbstractProcessor {
 
-    abstract public void handle(List<Annotation> annotations, T t);
+    abstract public void handle(Element element, T t);
+
+    @SuppressWarnings("all")
+    @Override
+    public final boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+
+        annotations.forEach((anno) -> {
+            for (Element element : roundEnv.getElementsAnnotatedWith(anno)) {
+
+                try {
+                    Class<? extends Annotation> clazz = (Class<? extends Annotation>) Class.forName(anno.getQualifiedName().toString());
+
+                    T t = (T) element.getAnnotation(clazz);
+                    if (t == null) throw new RuntimeException("Annotation is null.");
+
+                    handle(element, t);
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        return true;
+    }
 
     @Override
-    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        return false;
+    public final SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latest();
     }
 }
